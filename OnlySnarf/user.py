acfb58119e4b0fc973b4861a94b33f2efe0da995
skeetzo <brainlessdg13@gmail.com>
2019-03-25 10:44:46 -0700

    user.py

---
+++ OnlySnarf/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# 3/25/2019: Skeetzo
+# User Class
+
+class User:
+    def __init__(self, name, username):
+        self.name = name
+        self.username = username
+
+    def sendMessage(message, image, price):
+        pass
