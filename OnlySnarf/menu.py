d1f7cbc71663e1ecacfc8d593f2a89e7c26c817a
skeetzo <brainlessdg13@gmail.com>
2019-06-28 11:17:00 -0700

    1.1.6

--- OnlySnarf/menu.py
+++ OnlySnarf/menu.py
@@ -13,14 +13,14 @@ import datetime
 import json
 import sys
 import pathlib
-from .settings import SETTINGS as settings
-from . import onlysnarf
+from OnlySnarf.settings import SETTINGS as settings
+from OnlySnarf import onlysnarf
  
 ###################
 ##### Globals #####
 ###################
 
-version = "1.1.5"
+version = "1.1.6"
 header = "\n ________         .__          _________                     _____ \n \
 \\_____  \\   ____ |  | ___.__./   _____/ ____ _____ ________/ ____\\\n \
  /   |   \\ /    \\|  |<   |  |\\_____  \\ /    \\\\__  \\\\_   _ \\   __\\ \n \
