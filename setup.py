47c236ac297a302bac47eab1fc2e4bbed9a146a1
skeetzo <brainlessdg13@gmail.com>
2019-08-14 19:50:18 -0700

    1.1.9

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OnlySnarf",
-    version="1.1.8",
+    version="1.1.9",
     author="Skeetzo",
     author_email="WebmasterSkeetzo@gmail.com",
     url = 'https://github.com/skeetzo/onlysnarf',
-    download_url = 'https://github.com/skeetzo/onlysnarf/archive/v1.1.8.tar.gz',
+    download_url = 'https://github.com/skeetzo/onlysnarf/archive/v1.1.9.tar.gz',
     keywords = ['OnlyFans', 'Content', 'OnlySnarf'],
     description="OnlyFans Content Distribution Tool",
     long_description=long_description,
