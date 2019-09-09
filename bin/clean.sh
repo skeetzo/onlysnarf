7e1b0c2b21cf0854fb7852865d449674f087b5ce
Skeetzo <WebmasterSkeetzo@gmail.com>
2019-09-08 21:25:12 -0700

    cleanup

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm -rf dist/ build/ *.egg-info
-git filter-branch --tree-filter 'rm -rf /home/skeetzo/Projects/onlysnarf/OnlySnarf/profile.conf' HEAD
+git filter-branch --tree-filter 'rm -rf ./OnlySnarf/profile.conf' HEAD
