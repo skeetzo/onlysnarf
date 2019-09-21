86f1d70f07b04dbcf444ba894cf0dfbf526016da
Skeetzo <WebmasterSkeetzo@gmail.com>
2019-09-21 00:05:11 -0700

    1.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OnlySnarf",
-    version="1.4.2",
+    version="1.4.3",
     author="Skeetzo",
     author_email="WebmasterSkeetzo@gmail.com",
     url = 'https://github.com/skeetzo/onlysnarf',
