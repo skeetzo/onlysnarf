49b20bd37bc4f56e208fd533fa7bd55eec2b46a0
skeetzo <brainlessdg13@gmail.com>
2019-10-07 23:19:12 -0700

    2.8.0

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo cp ../onlysnarf/OnlySnarf/config.conf /etc/onlysnarf
+# sudo cp ../onlysnarf/OnlySnarf/config.conf /etc/onlysnarf
 sudo python3 setup.py install
-sudo onlysnarf -debug -show-window
+sudo onlysnarf -debug -show-window -debug-delay
