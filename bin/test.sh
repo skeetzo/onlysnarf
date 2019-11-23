344219b6761851166a4cf5210a13231d7343ec2f
Skeetzo <WebmasterSkeetzo@gmail.com>
2019-11-23 14:14:35 -0800

    2.14.2

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # sudo cp ../onlysnarf/OnlySnarf/config.conf /etc/onlysnarf
 sudo python3 setup.py install
-sudo onlysnarf -debug -verbose -show-window -debug-delay -prefer-local
+# sudo onlysnarf -debug -verbose -show-window -debug-delay -prefer-local
+sudo onlysnarfpy -verbose -type gallery -notkeyword feet -show-window -debug -debug-delay
