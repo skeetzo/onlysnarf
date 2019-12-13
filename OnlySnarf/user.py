2d3a753b0c8f6df8f0496ec3d73e662ce4c7349c
Skeetzo <WebmasterSkeetzo@gmail.com>
2019-12-12 19:51:54 -0800

    Saved

--- OnlySnarf/user.py
+++ OnlySnarf/user.py
@@ -155,7 +155,7 @@ class User:
     def get_active_users(Driver):
         if str(settings.PREFER_LOCAL) == "True": return read_users_local()
         active_users = []
-        users = Driver.user_get()
+        users = Driver.users_get()
         for user in users:
             try:
                 user = User(user)
