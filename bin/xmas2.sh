550f3969a2463ef0d8846d3123b12bffb606d67e
Skeetzo <WebmasterSkeetzo@gmail.com>
2019-12-25 21:15:09 -0800

    Saved

---
+++ bin/xmas2.sh
@@ -0,0 +1,8 @@
+sudo onlysnarfpy \
+-action "upload" \
+-type "gallery" \
+-text "xmas tease" \
+-image "/home/schizo/Documents/xmas tease" \
+-image-upload-limit 10 \
+-image-download-limit 10 \
+-force-upload
