6af5ab1c292596d7e33efa3b4d808659d12a02d8
Skeetzo <WebmasterSkeetzo@gmail.com>
2019-12-25 21:17:08 -0800

    Saved

--- bin/xmas2.sh
+++ bin/xmas2.sh
@@ -2,7 +2,8 @@ sudo onlysnarfpy \
 -action "upload" \
 -type "gallery" \
 -text "xmas tease" \
--image "/home/schizo/Documents/xmas tease" \
+-method "input" \
+-input "/home/schizo/Documents/xmas tease" \
 -image-upload-limit 10 \
 -image-download-limit 10 \
 -force-upload
