2caa6d4e575360f2004dcf1d6ba50a93369691dc
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-01-09 20:06:38 -0800

    Saved

---
+++ OnlySnarf/settings-profile.py
@@ -0,0 +1,500 @@
+
+    ## Settings ##
+
+    ## Account
+    # cover image enter
+    {
+        "name": "coverImage",
+        "classes": ["b-user-panel__cover__img"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # cover image cancel
+    {
+        "name": "coverImageCancel",
+        "classes": [],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # profile photo
+    {
+        "name": "profilePhoto",
+        "classes": ["g-btn.m-rounded.m-sm.m-border"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # display name
+    {
+        "name": "displayName",
+        "classes": ["form-control.g-input"],
+        "text": [],
+        "id": [],
+        "tabIndex": 7, # left
+        "from": "load"
+    },
+    # subscription price
+    {
+        "name": "subscriptionPrice",
+        "classes": ["form-control.g-input","classname"],
+        "text": [],
+        "id": [],
+        "tabIndex": 6, # left
+        "from": "load"
+    },
+    # ADD reward for subscriber referrals
+    # about
+    {
+        "name": "about",
+        "classes": ["form-control.g-input.unlimsize"],
+        "text": [],
+        "id": [],
+        "tabIndex": 5, # left
+        "from": "load"
+    },
+    # location
+    {
+        "name": "location",
+        "classes": ["form-control.g-input"],
+        "text": [],
+        "id": [],
+        "tabIndex": 4, # left
+        "from": "load"
+    },
+    # website url
+    {
+        "name": "websiteURL",
+        "classes": ["form-control.g-input"],
+        "text": [],
+        "id": [],
+        "tabIndex": 3, # left
+        "from": "load"
+    },
+
+    ## Advanced
+    # username
+    # BLANK
+    # email
+    {
+        "name": "email",
+        "classes": ["form-control.g-input"],
+        "text": [],
+        "id": [],
+        "tabIndex": 3, # left
+        "from": "load"
+    },
+    # username
+    {
+        "name": "username",
+        "classes": ["form-control.g-input"],
+        "text": [],
+        "id": [],
+        "tabIndex": 4, # left
+        "from": "load"
+    },
+    # connect other onlyfans accounts username enter area
+    # BLANK
+    # password
+    {
+        "name": "password",
+        "classes": ["form-control.g-input"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # password 2x
+    {
+        "name": "newPassword",
+        "classes": ["form-control.g-input"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # confirm new password
+    {
+        "name": "confirmPassword",
+        "classes": ["form-control.g-input"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+
+    ## Notifications
+    # push notifications
+    # BLANK
+    # email notifications
+    {
+        "name": "emailNotifs",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new referral email
+    {
+        "name": "emailNotifsNewReferral",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new stream email
+    {
+        "name": "emailNotifsNewStream",
+        "classes": ["b-input-radio"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new subscriber email
+    {
+        "name": "emailNotifsNewSubscriber",
+        "classes": ["b-input-radio"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new tip email
+    {
+        "name": "emailNotifsNewSubscriber",
+        "classes": ["b-input-radio"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new renewal email
+    {
+        "name": "emailNotifsNewSubscriber",
+        "classes": ["b-input-radio"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+
+    {
+        "name": "emailNotifsNewTip",
+        "classes": ["b-input-radio"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    #
+    {
+        "name": "emailNotifsRenewal",
+        "classes": ["b-input-radio"],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new likes summary
+    {
+        "name": "emailNotifsNewLikes",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new posts summary
+    {
+        "name": "emailNotifsNewPosts",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new private message summary
+    {
+        "name": "emailNotifsNewPrivMessages",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # telegram bot button
+    # BLANK
+    # site notifications
+    {
+        "name": "siteNotifs",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new comment notification
+    {
+        "name": "siteNotifsNewComment",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new favorite notification
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # discounts from users i've used to follow notification
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new subscriber notification
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # new tip notification
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # toast notification new comment
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # toast notification new favorite
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # toast notification new subscriber
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # toast notification new tip
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    ## Security
+    # two step toggle
+    # BLANK
+    # fully private profile
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # enable comments
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # show fans count on profile
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # show posts tips summary
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # public friends list
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # geo blocking
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": 4, # left
+        "from": "load"
+    },
+    # ip blocking
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": 3, # left
+        "from": "load"
+    },
+    # watermarks photos
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # watermarks video
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # watermarks text
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": 2,
+        "from": "load"
+    },
+    ####### save changes may be the same for each
+    ## Story
+    # allow message replies - nobody
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # allow message replies - subscribers
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # show story for - everyone
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    # show story for - subscribers
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None,
+        "from": "load"
+    },
+    ## Other
+    # obs server
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": 12, # left
+        "from": "load"
+    },
+    # obs key
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": 11, # left
+        "from": "load"
+    },
+    # welcome chat message toggle
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": 10, 
+        "from": "load"
+    },
+    # then same pattern for message enter text or add stuff and price
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None, # lefts
+        "from": "load"
+    },
+    # save button for welcome chat message
+    {
+        "name": "BALLS",
+        "classes": [BALLSBALLS],
+        "text": [],
+        "id": [],
+        "tabIndex": None, # left
+        "from": "load"
+    },
