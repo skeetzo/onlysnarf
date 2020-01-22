8e0fc667a02ab8217bd812e01245ab37f2fe0259
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-01-22 00:25:36 -0800

    2.16.4

--- bin/test.sh
+++ bin/test.sh
@@ -19,10 +19,10 @@ echo "##################################################" >> ../onlysnarf/logs/t
 # sudo onlysnarfpy -debug -verbose -verboser -verbosest -action message -method "favorite" -text "test pussy" -bykeyword "pussycats" -show-window
 # sudo onlysnarfpy -verbose -type gallery -notkeyword feet -show-window -debug -debug-delay
 # sudo onlysnarf-config
-# sudo onlysnarfpy -debug -verbose -action test
+sudo onlysnarfpy -debug -verbose -verboser -verbosest -action test
 # sudo onlysnarfpy -debug -verbose -show-window -type image -bykeyword "pussycats"
 
-sudo onlysnarfpy -debug -action post -text "shnarf!" -verbose -verboser -show-window
+# sudo onlysnarfpy -debug -action post -text "shnarf!" -verbose -verboser -show-window
 
 # # Message
 # sudo onlysnarfpy \
