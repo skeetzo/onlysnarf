04612fcadb1169e2377ca536275f648b3616510a
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-03-31 13:46:02 -0700

    almost there

--- bin/tests/upload.sh
+++ bin/tests/upload.sh
@@ -1,38 +1,37 @@
 #!/usr/bin/env bash
 
-# Image
-echo "[*] Upload - Image"
-sudo onlysnarfpy \
--debug \
--action "post" -category "image" \
--bykeyword "pussycats" \
--text "image testes" \
--debug-delay \
--show \
--verbose -verbose -verbose
-# -verbose
-# -verbose -verbose
+# # Image
+# echo "[*] Upload - Image"
+# sudo onlysnarfpy \
+# -debug \
+# -action "post" -category "image" \
+# -bykeyword "pussycats" \
+# -text "image testes" \
+# -debug-delay \
+# -show \
+# -verbose -verbose -verbose
+# # -verbose
+# # -verbose -verbose
 
-# Gallery
-echo "[*] Upload - Gallery"
-sudo onlysnarfpy \
--debug \
--action "post" -category "gallery" \
--bykeyword "pussycats" \
--text "gallery testes" \
--debug-delay \
--show \
--verbose -verbose -verbose
-# -verbose
-# -verbose -verbose
+# # Gallery
+# echo "[*] Upload - Gallery"
+# sudo onlysnarfpy \
+# -debug \
+# -action "post" -category "gallery" \
+# -bykeyword "pussycats" \
+# -text "gallery testes" \
+# -debug-delay \
+# -show \
+# -verbose -verbose -verbose
+# # -verbose
+# # -verbose -verbose
 
 # Video
 echo "[*] Upload - Video"
 sudo onlysnarfpy \
--debug \
 -action "post" -category "video" \
--bykeyword "pussycats" \
--text "video testes" \
+-bykeyword "stroke" \
+-text "fuck my upload speeds in the face" \
 -debug-delay \
 -show \
 -verbose -verbose -verbose
