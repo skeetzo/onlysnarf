7af4f9a93f324228eb1fdc211f70eb328a5b7bfe
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-04-03 04:31:47 -0700

    Saved

--- bin/tests/post.sh
+++ bin/tests/post.sh
@@ -6,16 +6,16 @@ sudo onlysnarfpy \
 -debug \
 -action "post" \
 -text "post testes" \
+-show \
 -verbose
 # -verbose -verbose -verbose
-# -show \
 # -verbose -verbose
 
 echo "[*] Post - Text"
 sudo onlysnarfpy \
 -debug \
 -action "post" \
+-show \
 -verbose
 # -verbose -verbose -verbose
-# -show \
 # -verbose -verbose
