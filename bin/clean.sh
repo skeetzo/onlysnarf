b4a78beeccf78b66bb04fb10a63550490c6297be
Skeetzo <WebmasterSkeetzo@gmail.com>
2020-06-12 23:18:09 -0700

    2.17.8

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-rm -rf dist/ build/ *.egg-info logs
+rm -rf dist/ build/ *.egg-info geckodriver.log
 mkdir ../onlysnarf/logs
 # git filter-branch -f --tree-filter 'rm -rf ./OnlySnarf/config.conf' HEAD
