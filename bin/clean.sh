b4cbc1ccd3eff419f3bb81256ac2fc9649ed486b
Skeetzo <WebmasterSkeetzo@gmail.com>
2022-09-14 23:01:26 -0700

    4.2.2

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-rm -rf dist/ build/ *.egg-info geckodriver.log log/*
 # git filter-branch -f --tree-filter 'rm -rf ./OnlySnarf/google_creds.txt' HEAD
 
+rm -rf dist/ build/ *.egg-info
+
 # project logs
-rm $HOME/OnlySnarf/snarf.log $HOME/.onlysnarf/log/*
+rm -rf log/* $HOME/OnlySnarf/snarf.log $HOME/.onlysnarf/log/* 
 
 # session data and cookies
-rm $HOME/.onlysnarf/session.json $HOME/.onlysnarf/cookies.pkl
+rm -rf $HOME/.onlysnarf/session.json $HOME/.onlysnarf/cookies.pkl
 
 # any remaining files
-rm $HOME/OnlySnarf/downloads/* $HOME/OnlySnarf/uploads/*
+rm -rf $HOME/OnlySnarf/downloads/* $HOME/OnlySnarf/uploads/*
