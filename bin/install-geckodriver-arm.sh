70c2aa051e6ca3446b49e4c8e519d9273a0bb0de
Skeetzo <WebmasterSkeetzo@gmail.com>
2022-10-20 13:10:17 -0700

    4.4.3

--- bin/install-geckodriver-arm.sh
+++ bin/install-geckodriver-arm.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# https://raspberrypi.stackexchange.com/questions/63258/selenium-firefox-oserror-errno-8-exec-format-error
 wget "https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-arm7hf.tar.gz" -O /tmp/geckodriver.tar.gz
 sudo tar -xvzf /tmp/geckodriver*
 sudo chmod +x ./geckodriver
