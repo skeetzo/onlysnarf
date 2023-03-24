e2c6b568ea3c447d9434c69bc00bbb9fa6401fc4
Skeetzo <WebmasterSkeetzo@gmail.com>
2023-03-24 12:00:35 -0700

    4.4.12
    Former-commit-id: a789f086adbcf0e43be2733e79c5588d9121150a
    Former-commit-id: 082bf7dcaecb82c27dc4394e10598abce0cd16b1 [formerly e6e9293a1a216dfb37859d3b26c6351a9b472381]
    Former-commit-id: 328b25644593c7bb85df82ebca72adb80d5b2d79

--- OnlySnarf/util/settings.py
+++ OnlySnarf/util/settings.py
@@ -543,6 +543,7 @@ class Settings:
 
     def set_debug(newValue):
         if str(newValue) == "tests":
+            pass
             # config["confirm"] = False
         else:
             config["debug"] = newValue
